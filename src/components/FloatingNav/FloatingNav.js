1793b9f53b28cc05f3d88c78520e07afed674fd5
Kashi Samaraweera <kashi@kashis.com.au>
2020-02-02 13:29:51 +0545

    Updated logo to fall inline with page margin.

--- src/components/FloatingNav/FloatingNav.js
+++ src/components/FloatingNav/FloatingNav.js
@@ -2,11 +2,15 @@ import React from 'react'
 import styles from './floatingnav.module.css'
 import Logo from '../../img/kashis-logo.png'
 
-const FloatingNav = ({ pages, landingRatio = 1 }) => {
+const FloatingNav = ({ pages, landingRatio = 1, isMobile = true }) => {
   const navScale = 0.5 + (landingRatio) * 0.5
+  const logoLockupDisplace = isMobile? (1-landingRatio) * 50 : 0;
+  const logoLockupMargin = isMobile? (1-landingRatio)  * -11 : 0;
   return (
     <nav className={styles.FloatingNav} style={{ transform: `scale(${navScale}) translateY(${landingRatio*5}em)`}}>
-      <span className={styles.logoLockup} title="Scroll to top">
+      {/* <span className={styles.logoLockup} style={{ transform: `translateX(calc(${logoLockupDisplace}vw + ${logoLockupMargin}rem))`}}> */}
+      <span className={styles.logoLockup} style={{ transform: `translateX(${logoLockupDisplace}vw) translateX(${logoLockupMargin}em)`}}>
+      {/* <span className={styles.logoLockup}> */}
         <img className={styles.logo} src={Logo} alt="Kashis.com.au's Logo" />
         <h1 className={styles.logoType}>Kashi Samaraweera</h1>
       </span>
